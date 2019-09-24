380d9403d2fe470246f6b634331464e41806cfd4
李静波 <lijingbo09@cnpc.com.cn>
2019-09-24 17:05:13 +0800

    创建 hystrix 断路器 添加监听器，监听请求数据

--- spring-cloud-eureka-user-hystrix/src/main/java/cc/lijingbo/hystrix/domain/Configration.java
+++ spring-cloud-eureka-user-hystrix/src/main/java/cc/lijingbo/hystrix/domain/Configration.java
@@ -1,5 +1,8 @@
 package cc.lijingbo.hystrix.domain;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +16,15 @@ public class Configration {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
